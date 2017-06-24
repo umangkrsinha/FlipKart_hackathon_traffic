f71aa29653f42008e11b017f309bd1fab55bb1de
umang <umangkrsinha@gmail.com>
2017-06-24 15:42:02 +0530

    changed the app.use line

--- web.js
+++ web.js
@@ -7,5 +7,5 @@ app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
 app.use(function(req, res){
-res.sendFile(__dirname + "/dist/index.html");
+res.sendFile(__dirname + "/dist/client/index.html");
 });
