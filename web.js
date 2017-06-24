4d7923bc5e15861f584c125bf55a5f1b87a7b1d9
umang <umangkrsinha@gmail.com>
2017-06-24 15:36:11 +0530

    added app.use line in web.js

--- web.js
+++ web.js
@@ -6,3 +6,6 @@ var app = express();
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
+app.use(function(req, res){
+res.sendFile(__dirname + "/dist/index.html");
+});
